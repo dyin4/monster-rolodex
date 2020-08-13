494907825eeabfbf2291bb307a033b7f80fd5ca4
Dan Yin <danyin@Dans-MacBook-Pro.local>
2020-08-12 22:19:06 -0700

    change img

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import './card.styles.css';
 
 export const Card = props => (
     <div className = 'card-container' >
-    <img alt="moster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x200`} />
+    <img alt="moster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
         <h2> {props.monster.name} </h2>
         <p> {props.monster.email}</p>
     </div>
